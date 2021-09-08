0d2eceeec43091e4f68c15d70f075efa6eae8ac3
Sergio Silva <sergiobtos@hotmail.com>
2021-09-08 12:17:11 -0400

    Services and Testimonials update

--- main.js
+++ main.js
@@ -27,3 +27,31 @@ window.addEventListener('scroll', function () {
     header.classList.remove('scroll')
   }
 })
+
+/* Swiper */
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
+/* ScrollReveal */
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+                    #about .image, #about .text,
+                    #services header, #services .card,
+                    #testimonials header, #testimonials .testimonials
+                    #contact .text, #contact .links 
+                    `,
+  { interval: 100 }
+)
